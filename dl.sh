0ca4716f429323e025393396c520e2aa9bff6c1c
Milan Andric <mandric@gmail.com>
2025-03-15 11:59:19 -0500

    dl.sh: supports args on cli

--- dl.sh
+++ dl.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-youtube_transcript_api DibH4GRRWQU --languages en --format srt
+# DibH4GRRWQU Lex and Guido
+vidId="${1:-ApQ9NovgnA4}"
+lang="${2:-en-US}"
+youtube_transcript_api "$vidId" --languages "$lang" --format srt
 
